379a59f9906ff98e1df58f38b483a18cf978c0e3
joey57 <njorogejoey5@gmail.com>
2022-04-29 15:25:11 +0300

    create news route

--- app/views.py
+++ app/views.py
@@ -9,3 +9,10 @@ def index():
   '''
   title = 'Catch up real quick news headlines'
   return render_template('index.html',title=title)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+  '''
+  view news page function that returns the news details page and its data
+  '''  
+  return render_template('index.html', id = news_id)
