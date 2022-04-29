4a4cf4ed4a00959d0b6e16e14393292081990d1b
joey57 <njorogejoey5@gmail.com>
2022-04-29 21:02:35 +0300

    added more categories

--- app/views.py
+++ app/views.py
@@ -13,8 +13,11 @@ def index():
   all_news = get_news('sports')
   general_news = get_news('general')
   tech_news = get_news('technology')
+  bus_news = get_news('business')
+  ent_news = get_news('ent')
+  sci_news = get_news('science')
 
-  return render_template('index.html',title=title, sports = all_news, general = general_news, technology = tech_news)
+  return render_template('index.html',title=title, sports = all_news, general = general_news, technology = tech_news,  business = bus_news, ent = ent_news, science = sci_news)
 
 @app.route('/news/<int:id>')
 def news(id):
