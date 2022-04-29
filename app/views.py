ab27656b01d2370f22b4e7d9dc96c168f8fca029
joey57 <njorogejoey5@gmail.com>
2022-04-29 15:09:13 +0300

    display views title

--- app/views.py
+++ app/views.py
@@ -7,5 +7,5 @@ def index():
   '''
   view root page function that returns the index page and its data
   '''
-  return render_template('index.html')
-  
+  title = 'Catch up real quick news headlines'
+  return render_template('index.html',title=title)
