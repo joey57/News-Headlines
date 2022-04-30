88c66fd193d2d6114f250eed24291adb8a441eab
joey57 <njorogejoey5@gmail.com>
2022-04-30 12:43:17 +0300

    added more test

--- app/test/test_articles.py
+++ app/test/test_articles.py
@@ -1,16 +1,25 @@
 import unittest
 from app.models import Articles
 
-class TestArticle(unittest.TestCase):
+class TestArticles(unittest.TestCase):
     '''
+    Test class to test the behavior of the articles class
     '''
     def setUp(self):
         '''
+        Test class to run before other tests
         '''
-        self.new_article =Articles('abc','lorem ipsum','image','12-24-2018','liz','abc.com')
+        self.new_article = Articles('Title goes here','Title is great ...','https://google.com/images','2018-05-12T13:31:03Z', 'liz', 'buzzfeed.com')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_article, Articles))    
+        self.assertTrue(isinstance(self.new_article,Articles))
 
+    def test_to_check_instance_variables(self):
+        self.assertEquals(self.new_article.title,'Title goes here')
+        self.assertEquals(self.new_article.description,'Title is great...')
+        self.assertEquals(self.new_article.image,'https://google.com/images')
+        self.assertEquals(self.new_article.publishedAt,'h2018-05-12T13:31:03Z')
+        self.assertEquals(self.new_article.author,'liz')
+        self.assertEquals(self.new_article.url,'buzzfeed.com')
 
     
--- app/test/test_news.py
+++ app/test/test_news.py
@@ -1,14 +1,28 @@
 import unittest
 from app.models import News
 
-
-class TestNewsSource(unittest.TestCase):
+class TestNews(unittest.TestCase):
     '''
+    Test class to test the behavior of the articles class
     '''
     def setUp(self):
         '''
+        Test class to run before other tests
         '''
-        self.new_newssource =News('abc','liz','liz.com','lorem ipsum','kenya','technology', 'abc-news')
+        self.new_news_source = News('123','Misha','https://123.com/','123 news is the best source', 'london', 'general', '123-news')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_newssource, News))
+        '''
+        '''
+        self.assertTrue(isinstance(self.new_news_source,News))
+
+    def test_to_check_instance_variables(self):
+        '''
+        '''
+        self.assertEquals(self.new_news_source.name,'123')
+        self.assertEquals(self.new_news_source.author,'Misha')
+        self.assertEquals(self.new_news_source.url,'https://123.com/')
+        self.assertEquals(self.new_news_source.description,'123 news is the best source')
+        self.assertEquals(self.new_news_source.country,'london')
+        self.assertEquals(self.new_news_source.category,'general')
+        self.assertEquals(self.new_news_source.id,'123-news')
